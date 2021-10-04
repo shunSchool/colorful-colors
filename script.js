02e2e6b9800fe8ff73c50ba098aca691d8b28105
shunSchool <72032942+shunSchool@users.noreply.github.com>
2021-10-04 11:30:18 -0600

    start

---
+++ script.js
@@ -0,0 +1,10 @@
+//color array
+
+//variables for HTML elements
+let containerEl = document.getElementById("container");
+
+//array
+let colors = ["red", "green", "blue"];
+// index ---> 0       1        2
+
+containerEl.innerHTML = "<div>" + colors[0] + "</div>";
